2d6f064f72cb18151ec41412f2deb740d8fbc9c9
DLC <shabrov23.02@gmail.com>
2024-05-14 18:14:29 +0300

    hex: not work

--- config/config_test.go
+++ config/config_test.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadConfig(t *testing.T) {
+	configContent := `{
+		"telegramToken": "test_token",
+		"telegramChatID": 123456789,
+		"mappingFile": "file_mapping.txt"
+	}`
+	configFile := "test_config.json"
+	err := os.WriteFile(configFile, []byte(configContent), 0644)
+	assert.NoError(t, err)
+	defer os.Remove(configFile)
+
+	config, err := LoadConfig(configFile)
+	assert.NoError(t, err)
+	assert.Equal(t, "test_token", config.TelegramToken)
+	assert.Equal(t, int64(123456789), config.TelegramChatID)
+	assert.Equal(t, "file_mapping.txt", config.MappingFile)
+}
